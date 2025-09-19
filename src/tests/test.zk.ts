1d4aa1cb9153b8ff2842f95a6edf38bfba2b1b6c
kyngwan@gmail.com <kyngwan@gmail.com>
2025-09-19 18:00:22 +0900

    chore: enable ase-256-gcm

--- src/tests/test.zk.ts
+++ src/tests/test.zk.ts
@@ -20,14 +20,10 @@ import {
 const ZK_CIPHER_SUITES: CipherSuite[] = [
   "TLS_CHACHA20_POLY1305_SHA256",
   "TLS_AES_128_GCM_SHA256",
-  // 'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384',
+  "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
 ];
 
-const ZK_ENGINES: ZKEngine[] = [
-  // 'gnark',
-  // 'snarkjs',
-  "barretenberg",
-];
+const ZK_ENGINES: ZKEngine[] = ["gnark", "snarkjs", "barretenberg"];
 
 type RedactionTestVector = {
   input: string[];
